7c5500e59891914f8dd3937631c33310d593c20f
Gemma Batlle <gembb15@gmail.com>
2022-11-30 12:10:05 +0100

    adding content from the API to home page

--- js/app.js
+++ js/app.js
@@ -4,7 +4,11 @@ async function fetchedProjects(){
     const response = await fetch(API_URL);
     const data = await response.json();
 
-    const sortedData = data.sort((item1, item2) => {
+    addProjects(data)
+}
+
+function addProjects(projects){
+    const sortedData = projects.sort((item1, item2) => {
         if (item1.uuid < item2.uuid) {
             return -1;
         }
@@ -16,7 +20,18 @@ async function fetchedProjects(){
 
     const threeFirstProjects = sortedData.slice(0, 3);
 
-    console.log(threeFirstProjects)
+    threeFirstProjects.forEach(project => {
+        const divPrincipal = document.getElementById("divPrincipal");
+        const div = `
+            <div class="bg-white my-8 md:w-96 shadow-lg">
+                <img class="md:h-60 md:w-full" src="${project.image}" alt="${project.name}">
+                <h4 class="medium-24 color-dark-grey">${project.name}</h4>
+                <p class="md:regular 20 regular 17 color-medium-grey m-3">${project.description}</p>
+                <a class="regular-17 color-dark-blue cursor-pointer" href="uuid=${project.uuid}">Learn More</a>
+            </div>
+        `
+        divPrincipal.innerHTML += div;
+    });
 }
 
 
