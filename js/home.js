96eb50ba3d9828d5c7e0a2678f2371a1a4110549
jewlys <jewlys@hotmail.com>
2022-12-01 13:28:28 +0100

    mmc

--- js/home.js
+++ js/home.js
@@ -16,4 +16,31 @@
       bar3.classList.toggle('rotate-45');
   });
   
- 
+  function message(){
+    var Name = document.getElementById('name');
+    var email = document.getElementById('email');
+    var phone = document.getElementById('phone'); 
+    var message = document.getElementById('message');
+    const success = document.getElementById('success');
+    const danger = document.getElementById('danger');
+
+    if(Name.value === '' || email.value === '' || message.value === ''){
+        danger.style.display = 'block';
+    }
+    else{
+        setTimeout(() => {
+            Name.value = '';
+            email.value = '';
+            msg.value = '';
+        }, 2000);
+
+        success.style.display = 'block';
+    }
+
+
+    setTimeout(() => {
+        danger.style.display = 'none';
+        success.style.display = 'none';
+    }, 4000);
+
+}
