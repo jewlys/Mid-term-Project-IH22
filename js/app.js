991d0669d123f44f783fc4098b22bb44dfd98f76
jewlys <jewlys@hotmail.com>
2022-12-01 11:56:47 +0100

    nn

--- js/app.js
+++ js/app.js
@@ -40,5 +40,37 @@ window.addEventListener('load', () => {
     fetchedProjects()
 })
 
-
+const contactForm = (preventForm) => {
+    preventForm.preventDefault();
+    let name = document.querySelector("#name").value;
+    let email = document.querySelector("#email").value;
+    let phone = document.querySelector("#phone").value;
+    let textarea = document.querySelector("#text").value;
+    let formComfirmation = "";
+    fetch("https://raw.githubusercontent.com/ironhack-jc/mid-term-api/main/projects-v2", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+        Accept: "text/plain, application/json",
+      },
+        body: JSON.stringify({
+            name: name,
+            email: email,
+            phone: phone,
+            text: textarea
+        }),
+    })
+      .then((response) => response.json())
+      .then((formData) => console.log(formData))
+      .then(() => {
+        formComfirmation += `
+                <h3 class="comfirmation">
+                    Thank you for reaching us! We'll answer to you soon.
+            </h3>
+                `;
+        document.querySelector(".success").innerHTML += formComfirmation;
+      })
+      .catch((error) => console.log(error));
+  };
+  document.querySelector("#form").addEventListener("submit", contactForm);
 
